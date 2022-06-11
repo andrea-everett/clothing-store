0c90227b72c0585a8f212ee8295701a4d0895177
andrea-everett <andrea.dawn.everett@gmail.com>
2022-06-11 16:26:18 -0400

    toggle

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,18 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { signOutUser } from '../../utils/firebase.utils';
 
 import { UserContext } from '../../context/user.context';
-
+import { CartContext } from '../../context/cart.context';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 
 const Navigation = () => {
    const { currentUser  } = useContext(UserContext);
+   const { isCartOpen } = useContext(CartContext);
 
     return (
       <Fragment>
@@ -32,7 +35,9 @@ const Navigation = () => {
                        SIGN IN
                       </Link>
                    ) }
+                   <CartIcon />
              </div>
+             { isCartOpen && <CartDropdown  />}
           </div>
           <Outlet />
       </Fragment>
